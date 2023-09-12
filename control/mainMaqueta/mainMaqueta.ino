a0722b631351d5fcd4db02e1101657edda46779d
brianlorenzo <isebbrianlorenzo@gmail.com>
2023-09-12 14:39:00 -0300

    maqueta Main según lo acordado

---
+++ control/mainMaqueta/mainMaqueta.ino
@@ -0,0 +1,31 @@
+
+//Incluir librerias
+//  defines.h
+//  funciones.h
+//  auxiliares.h
+
+
+
+void setup() {
+
+  //  pinConfig()
+  //  comConfig()
+  //  otrosConfig()
+
+  // chequeoMecanismos() que se puedan usar
+
+  // comunicacionInit() establezco comunicación
+
+}
+
+void loop() {
+
+  // darInicioAJuego()
+  
+  // mantenerJuego()
+    //  init()
+    // jugar()
+
+  // resumen() terminó el juego
+    
+}
